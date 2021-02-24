b5ca52b469df05470a4402e3e3f2db96ace5d05e
Sobhan <stizer1374@gmail.com>
2021-02-24 13:04:44 +0330

    Install_iis,create directory and site

--- IIS_Automation.ps1
+++ IIS_Automation.ps1
@@ -2,25 +2,28 @@ Configuration Webserver
 {
 
   param (
- 
     [string]$port =$(Read-Host -Prompt 'Enter the your website port (default port is 8060)') ,
 
+    [string]$WebsiteName =$(Read-Host -Prompt 'Enter your website name (default name is ArianWeb)') ,
+
     [string]$arianwebpath =$(Read-Host -Prompt 'Enter Arianweb installation path (default path is c:\Arianweb)')
-
-
-
     )
 
-
-  if ([string]::IsNullOrWhiteSpace($port)){
-      
+  if ([string]::IsNullOrWhiteSpace($port))
+        {    
         $port = '8060'
-         }
+        }
 
-  if ([string]::IsNullOrWhiteSpace($arianwebpath)){
-      
+  if ([string]::IsNullOrWhiteSpace($arianwebpath))
+        {
         $arianwebpath = 'c:\Arianweb'
-         }
+        }
+
+  if ([string]::IsNullOrWhiteSpace($WebsiteName))
+        {
+        $WebsiteName = 'ArianWeb'
+        }
+
 
   Import-DscResource -ModuleName xwebadministration
   Import-DscResource -ModuleName PSDesiredStateConfiguration
@@ -39,13 +42,14 @@ Configuration Webserver
         "Web-Dir-Browsing","Web-Http-Errors","Web-Static-Content","Web-Http-Redirect", `
         "Web-Health","Web-Http-Logging","Web-Performance","Web-Stat-Compression","Web-Security", `
         "Web-Filtering","Web-App-Dev","Web-Net-Ext","Web-Net-Ext45","Web-Asp-Net","Web-Asp-Net45", `
-        "Web-ISAPI-Ext","Web-ISAPI-Filter","Web-Mgmt-Tools","Web-Mgmt-Console")){
-            
+        "Web-ISAPI-Ext","Web-ISAPI-Filter","Web-Mgmt-Tools","Web-Mgmt-Console"))
+                    {
                         WindowsFeature "$Feature"
                         {
                             Ensure = "present"
                             Name = $Feature
-                        }}
+                        }
+                    }
 # ... End loop
         # Create Directory
         File ArianWeb
@@ -67,10 +71,11 @@ Configuration Webserver
             DependsOn       = "[WindowsFeature]$Feature"
         }
 
-          xWebsite Arianweb
+        # Create Webiste
+        xWebsite Arianweb
          {
             Ensure          = 'Present'
-            Name            = 'Arian'
+            Name            = $WebsiteName
             State           = 'Started'
             PhysicalPath    = $arianwebpath
             BindingInfo     = @( MSFT_xWebBindingInformation
@@ -78,19 +83,16 @@ Configuration Webserver
                                    Protocol              = "HTTP"
                                    Port                  = $port
                                  }
-
                                 )
             DependsOn       = '[File]ArianWeb'
-
-         } 
-        
+         }       
     }
 }
+
 #create MOF file in Desire path
 Webserver -OutputPath "C:\DscConfiguration"
 #Running Configuration
 Start-DscConfiguration -wait -verbose -Path "C:\DscConfiguration"
 
-Start-DscConfiguration -Path 'C:\EnvironmentVariable_Path' -Wait -Verbose	Read MOF file and start configuration
 
 
