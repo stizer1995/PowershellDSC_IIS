7c308e3e648c934878640cf96b2f05e3dda08f35
sobhan <stizer1374@gmail.com>
2021-03-02 17:17:28 +0330

    install choco and your app

---
+++ Chocolatey.ps1
@@ -0,0 +1,24 @@
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+#Find-Module -Name cchoco | Install-Module -Force
+Configuration choco{
+
+    Import-DscResource -ModuleName cChoco
+    node localhost{
+
+        cChocoInstaller installChoco
+        {
+            InstallDir = "c:\ProgramData\chocolatey"
+        }
+        
+        cChocoPackageInstaller dotnetcore-windowshosting
+        {
+            Name = "dotnetcore-windowshosting"
+            DependsOn = "[cChocoInstaller]installChoco"
+        }
+        
+    }   
+    }
+    #create MOF file in Desire path
+    choco -OutputPath "C:\DscConfiguration\choco"
+    #Running Configuration
+    Start-DscConfiguration -wait -verbose -Path "C:\DscConfiguration\choco"
